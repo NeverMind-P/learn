da33c696788ef62adc170d95f1b2dae0602c5e9c
NeverMind <cfif2003boiko@gmail.com>
2019-03-27 13:25:03 +0200

    add button, js!

---
+++ main.js
@@ -0,0 +1,10 @@
+var  a = prompt("What's your name?", " ");
+if (a == "roma") {
+    alert("tu dodik");
+}
+else if(a == null) {
+        alert("error");
+    }
+else if (a!==null || a!=="roma"){
+        alert("Welcome");
+    }
